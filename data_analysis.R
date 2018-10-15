1ceddce9afe58f319f1921557a96e310f8d40d74
xeliot <daveho8888@gmail.com>
2018-10-15 13:56:49 -0700

    sample pie chart and data analysis on death count

--- data_analysis.R
+++ data_analysis.R
@@ -1,1 +1,8 @@
+library(dplyr)
+
 opioid_data <- read.csv("CDCOpioidData.csv", header=TRUE)
+opioid_death_count_by_state <- aggregate(opioid_data$Deaths, by=list(Category=opioid_data$State), FUN=sum)
+
+x <- opioid_death_count_by_state$x
+labels <- opioid_death_count_by_state$Category
+pie(x, labels)
